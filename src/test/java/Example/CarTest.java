89f0aa5ffbccc66fd724329ad8b4ae44000a26ae
Agnieszka Harłozińska <AHarlozinska@advaoptical.com>
2020-04-20 18:41:53 +0200

    car tests

--- src/test/java/Example/CarTest.java
+++ src/test/java/Example/CarTest.java
@@ -53,6 +53,38 @@ public class CarTest {
         assertFalse(myFerrari.needsFuel()); // Asercja nie powinna znajdować się z verify, ten test ma głównie za zadanie sprawdzenie czy atrapa wykonała metodę. Zbędne jest tutaj użycie asercji.
     }
 
+    //NOWE TESTY
+    @Test
+    public void test_stubbing_temperature(){
+        when(myFerrari.getEngineTemperature()).thenReturn(40.0);
+        assertEquals(myFerrari.getEngineTemperature(), 40.0);
+    }
+    @Test
+    public void test_temperature_exception() {
+        doThrow(new RuntimeException()).when(myFerrari).getEngineTemperature();
+        assertThrows(RuntimeException.class, () -> {
+            myFerrari.getEngineTemperature();
+        });
+    }
+    @Test
+    public void test_drive_to_null_exception() {
+        doThrow(new IllegalArgumentException()).when(myFerrari).driveTo(null);
+        assertThrows(IllegalArgumentException.class, () -> myFerrari.driveTo(null));
+    }
+    @Test
+    public void test_not_used_function() {
+        when(myFerrari.needsFuel()).thenReturn(true);
+        verify(myFerrari,never()).needsFuel();
+    }
+
+    @Test
+    void test_multiple_calls(){
+        when(myFerrari.getEngineTemperature()).thenReturn(40.0);
+        myFerrari.getEngineTemperature();
+        myFerrari.getEngineTemperature();
+        myFerrari.getEngineTemperature();
+        verify(myFerrari, times(3)).getEngineTemperature();
+    }
 
     @AfterEach
     public void tearDown(){
